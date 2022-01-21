481653c82f8c523d3b109fb96621eb2f43943468
Gustavo Medeiros <gustavojmmartins@outlook.com>
2022-01-20 22:07:39 -0300

    Começando curso JavaScript - programando a orientação a objetos

---
+++ Alura/JS-OO/index.js
@@ -0,0 +1,37 @@
+class Cliente {
+    nome
+    cpf
+}
+
+class ContaCorrente {
+    agencia
+    saldo
+
+    sacar(valor) {
+        if(this.saldo >= valor){ 
+            this.saldo -= valor
+        }/* o this serve para falar que é dessa conta corrente (a conta corrente da qual a função está dentro) */
+    }
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = "Ricardo"
+cliente1.cpf = 11122233309
+
+const cliente2 = new Cliente()
+cliente2.nome = "Alice"
+cliente2.cpf = 88822233309
+
+const contaCorrenteRicardo = new ContaCorrente()
+contaCorrenteRicardo.agencia = 1001
+contaCorrenteRicardo.saldo = 0
+
+console.log(contaCorrenteRicardo.saldo)
+contaCorrenteRicardo.saldo = 100
+console.log(contaCorrenteRicardo.saldo)
+contaCorrenteRicardo.sacar(200)
+ 
+
+console.log(contaCorrenteRicardo.saldo)
+console.log(cliente1)
+console.log(cliente2)
