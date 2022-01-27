6ee7071abd649b345156b7ed907b44b240bbaab0
Gustavo Medeiros <gustavojmmartins@outlook.com>
2022-01-26 23:12:50 -0300

    Atualizando código com herança e classses abstratas

--- Alura/JS-IH-OO/ContaPoupanca.js
+++ Alura/JS-IH-OO/ContaPoupanca.js
@@ -1,27 +1,12 @@
-export class ContaPoupanca {
+import {Conta} from "./Conta.js"
+
+export class ContaPoupanca extends Conta{
     constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial
-        this._cliente = cliente
-        this._agencia = agencia
+        super(saldoInicial, cliente, agencia)
     }
 
     sacar(valor) {
-        if(this._saldo >= valor){ 
-            this._saldo -= valor
-            return valor
-        }/* o this serve para falar que é dessa conta corrente (a conta corrente da qual a função está dentro) */
-    }
-
-    depositar(valor) {
-        if(valor <= 0) {
-            return
-        }
-
-        this._saldo += valor
-    }
-
-    transferir(valor, conta) {
-        const valorSacado = this.sacar(valor)
-        conta.depositar(valorSacado)
+        const taxa = 1.02
+        return this._sacar(valor, taxa)
     }
 }
